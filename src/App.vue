a459605e1ab398deb36196e9cdb7b51ba21f4fe3
n2ptune <n2ptune@outlook.com>
2020-01-15 19:38:54 +0900

    test

--- src/App.vue
+++ src/App.vue
@@ -3,3 +3,13 @@
     Test
   </div>
 </template>
+
+<style scoped>
+.root {
+  display: flex;
+  width: 100vw;
+  height: 100vh;
+  transform: translateY(-30%);
+  background-color: whitesmoke;
+}
+</style>
