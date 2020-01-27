cbe57697f93f8ccd46d1ecd56c028e513a16c2d6
n2ptune <n2ptune@outlook.com>
2020-01-27 21:44:20 +0900

    block rendering sidebar/header when guard navigation

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="font-display">
     <div class="root" v-if="!$route.meta.loginLayout">
-      <SideBar />
-      <Header />
+      <SideBar v-if="account" />
+      <Header v-if="account" />
       <router-view class="ml-56 bg-gray-200 h-screen overflow-y-auto pt-16" />
     </div>
     <div class="login-layout-root" v-else>
@@ -12,13 +12,17 @@
 </template>
 
 <script>
-import { mapState } from 'vuex'
+import { mapGetters } from 'vuex'
 
 export default {
   components: {
     SideBar: () => import('@/components/layout/SideBar'),
     Header: () => import('@/components/layout/Header')
   },
-  computed: mapState(['isViewSidebar'])
+  computed: {
+    ...mapGetters({
+      account: 'auth/getAccount'
+    })
+  }
 }
 </script>
